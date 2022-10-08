1ef66f78787657817f076efa1544fd2f83c77d03
Kryth <kendrickmallare.km@gmail.com>
2022-10-08 18:43:03 +0800

    added error check and manuevers, no design

--- mapquest/frontend.php
+++ mapquest/frontend.php
@@ -15,10 +15,86 @@
             $json_data = json_decode($json, true);
 
             # Get the Error Code, if 0 then its good, otherwise, display error
+            $scode = $json_data["info"]["statuscode"];
+            if($scode != 0 )
+            {
+                echo "<script>
+                if(window.confirm(\"There's been an issue, you got an error code of: $scode, click OK to be redirected to find troubleshoot the error\"))
+                {
+                    window.location.href = \"https://developer.mapquest.com/documentation/directions-api/status-codes\";
+                }
+                else
+                {
+                    window.location.href = \"input.php\";
+                }</script>";
+            }
+
+            # setting manuevers as an array variable, $manueverlist is the variable as a 2D array
+            # to get the values in $manueverlist its $manueverlist[x]["key"]
+            # x is the value and key is the keyword, it being "image", "distance", "direction", and "narrative"
+             $manueverlist = array();
+            foreach($json_data["route"]["legs"][0]["maneuvers"] as $manuever)
+            {
+                # distance in km with 2 decimal places
+                $manueverdistance = number_format($manuever["distance"] * 1.61, 2);
+
+                # check if final manuever, if so, its final destination
+                if($manueverdistance == 0)
+                {
+                    # map image
+                    $manueverimage = "N/A";
+                    # direction
+                    $manueverdirection = "Arrived";
+                    # narrative
+                    $manuevernarrative = $manuever["narrative"];
+                }
+                else
+                {
+                    # map image
+                    $manueverimage = $manuever["mapUrl"];
+                    # direction
+                    $manueverdirection = $manuever["directionName"];
+                    # narrative
+                    $manuevernarrative = $manuever["narrative"];
+                }
+
+                $manuevers = array(
+                    "image" => $manueverimage,
+                    "distance" => $manueverdistance,
+                    "direction" => $manueverdirection,
+                    "narrative" => $manuevernarrative
+                );
+                array_push($manueverlist, $manuevers);
+            }
+            # uncomment below to see the array
+            # print_r($manueverlist);
+
             # remove echo and add in front end, use the php variables to get the strings
-            echo $statuscode = "Status Code: " . $json_data["info"]["statuscode"] . "<br>";
+            echo $statuscode = "Status Code: $scode<br>";
             echo $startloc = "Starting Location: " . $json_data["route"]["locations"][0]["adminArea5"] . " " . $json_data["route"]["locations"][0]["adminArea1"] . "<br>";
             echo $dest = "Destination: " . $json_data["route"]["locations"][1]["adminArea5"] . " " . $json_data["route"]["locations"][1]["adminArea1"] . "<br>";
+            
+
+            # display manuevers, $manueverlist is the variable for its a 2D array
+            # to get the values in $manueverlist its $manueverlist[x]["key"]
+            # x is the value and key is the keyword, it being "image", "distance", "direction", and "narrative"
+            # if you have another idea go, but the structure of the variable is just 
+            # $manueverlist[x]["key"], where x is the row and key is the column name
+            foreach($manueverlist as $manuever)
+            {
+                # distance of 0 means they have arrived
+                if($manuever["distance"] == 0)
+                {
+                    echo "<br><br> You have reached your destination";
+                    break;
+                }
+
+                echo "<br><br>";
+                echo "<img src=\"" . $manuever["image"] . "\">";
+                echo "<br> Distance = " . $manuever["distance"] . " km";
+                echo "<br> Direction = " . $manuever["direction"];
+                echo "<br> Narrative = " . $manuever["narrative"];
+            }
         ?>
     </body>
 </html>
