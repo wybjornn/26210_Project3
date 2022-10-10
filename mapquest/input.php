3138e0b9051314e80d12d445b3b46c9c56077bad
PatriciaDelRosario <mapatricia.delrosario.iics@ust.edu.ph>
2022-10-10 16:43:01 +0800

    INPUT HTML CSS

--- mapquest/input.php
+++ mapquest/input.php
@@ -1,11 +1,111 @@
 <html>
+    <head>
+        <style>
+
+            * {
+                }
+
+            body {
+                background-image:url(img/map3.jpg);
+                background-repeat: no-repeat;
+                background-size: cover;
+                }
+
+            header {
+                background color: powderblue;
+                padding: 30px;
+                margin-bottom: 25px;
+                text-align: center;
+                }
+
+            footer {
+                background color: powderblue;
+                width: 100%;
+                padding: 15px;
+                margin-top: 100px;
+                margin-bottom: 0; 
+                text-align: center;
+
+                }
+
+            h1 {
+                font-family: 'Courier New', monospace;
+                font-size: 70px;
+                font-color: black;
+                margin-top: 80px;
+                }
+
+                h3 {
+                font-family: 'Courier New', monospace;
+                font-size: 30px;
+                font-color: black;
+                }
+
+                div {
+                border: double; 
+                border-width: 10px;
+                background-color: powderblue;
+                padding: 20px;
+                margin-left: 20%;
+                margin-right: 20%; 
+                }
+
+            input[type=text] {
+                width: 50%;
+                padding: 12px 20px;
+                margin: 8px 0;
+                display: inline-block;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+                box-sizing: border-box;
+                }
+
+            input[type=submit] {
+                width: 50%;
+                background-color: #4CAF50;
+                color: white;
+                padding: 14px 20px;
+                margin: 8px 0;
+                border: none;
+                border-radius: 4px;
+                cursor: pointer;
+                }
+
+            input[type=submit]:hover {
+                width: 50%;
+                background-color: black;
+                color: white;
+                padding: 14px 20px;
+                margin: 8px 0;
+                border: none;
+                border-radius: 4px;
+                cursor: pointer;
+                }
+                
+            
+        </style>
+    </head>
+
     <body>
-        <form action="frontend.php" method="get">
-            <label for="StartLoc">Starting Location:</label>
-            <input type="text" id="StartLoc" name="StartLoc"><br><br>
-            <label for="Dest">Destination:</label>
-            <input type="text" id="Dest" name="Dest"><br><br>
-            <input type="submit" value="Submit">
-        </form>
+        <center>
+        <header>
+            <h1> Know Your Map</h1>
+        </header>
+
+        <div>
+            <form action="frontend.php" method="get">
+                <label for="StartLoc"><h3>Starting Location: </h3> </label>
+                <input type="text" id="StartLoc" name="StartLoc" placeholder="Manila PH..."><br><br>
+                <label for="Dest"><h3>Destination: </h3></label>
+                <input type="text" id="Dest" name="Dest" placeholder="Tagaytay PH..."><br><br>
+                <input type="submit" value="Submit">
+            </form>
+        </div>
+
+        <footer>
+            <p>26210: Group Starbs</p>
+        </footer>
+        </center>
     </body>
+
 </html>
